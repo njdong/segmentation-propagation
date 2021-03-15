ad5cbd9fdf649abfca227676eac80ff66e26d5d4
Jilei Hao <jilei.cs@gmail.com>
2021-03-15 09:53:11 -0400

    Preprocessing logic

--- src/python/propagation.py
+++ src/python/propagation.py
@@ -1,5 +1,7 @@
 import os
+import vtk
 from Dicom4D import Dicom4D
+
 
 def propagate(fndcm, outdir = "", tag = "", seg_ref = "", framenums = "", fref = ""):
 
@@ -20,14 +22,43 @@ def propagate(fndcm, outdir = "", tag = "", seg_ref = "", framenums = "", fref =
     Process Inputs:
     - Load cartesian dicom image
     - Process reference segmentation
-        - Dialate the reference segmentation
+        - Dilate the reference segmentation
         - Create vtk mesh based on dialted reference segmentation
     - Extract and dialate 3D frame from the 4D image
 
     """
 
+    # directory storing temporary files
+    tmpdir = os.path.join(outdir, "tmp")
+
     # Use the  reader to create a Dicom4D Image
     CartesianDicom = Dicom4D(fndcm)
+
+    # Process reference segmentation
+    # - Dilate the reference segmentation (mask)
+    fn_mask_ref_srs = os.path.join(tmpdir, f'mask_{fref:02d}_{tag}_srs.nii.gz')
+    cmd = f"/usr/local/bin/c3d -int 0 {seg_ref} -threshold 1 inf 1 0 \
+        -dilate 1 10x10x10vox -resample 50% -o {fn_mask_ref_srs}"
+    print("Dilating reference segmentation...")
+    print(cmd)
+    os.system(cmd)
+
+    # - Create vtk mesh
+    fn_mask_ref_vtk = os.path.join(tmpdir, f'mask_{fref:02d}_{tag}.vtk')
+    cmd = f"vtklevelset {seg_ref} {fn_mask_ref_vtk} 1"
+    print("Creating mask mesh...")
+    print(cmd)
+    os.system(cmd)
+
+    # - Create vtk mesh from the dilated mask
+    fn_mask_ref_srs_vtk = os.path.join(tmpdir, f'mask_{fref:02d}_{tag}_srs.vtk')
+    cmd = f"vtklevelset {fn_mask_ref_srs} {fn_mask_ref_srs_vtk} 1"
+    print("Creating dilated mask mesh...")
+    print(cmd)
+    os.system(cmd)
+
+
+
     # Export 3D Frames
     # CartesianDicom.Export4D(os.path.join(outdir, 'img4D.nii.gz'))
     for i in framenums:
@@ -35,10 +66,13 @@ def propagate(fndcm, outdir = "", tag = "", seg_ref = "", framenums = "", fref =
         fnImgRs = f'{outdir}/img{i:02d}_{tag}_srs.nii.gz'
         CartesianDicom.ExportFrame(i, fnImg)
         
-        syscmd = '/usr/local/bin/c3d ' + fnImg + ' -smooth 1mm -resample 50% \-o ' + fnImgRs
-        print(syscmd)
-        os.system(syscmd)
+        cmd = '/usr/local/bin/c3d ' + fnImg + ' -smooth 1mm -resample 50% \-o ' + fnImgRs
+        print(cmd)
+        os.system(cmd)
 
+    # Preserving data
+    poly_data = vtk_read_polydata(fn_mask_ref_vtk)
+    print("Mesh data preserved")
 
     # Propagate Forward
     
@@ -50,5 +84,13 @@ def propagate(fndcm, outdir = "", tag = "", seg_ref = "", framenums = "", fref =
 
     # Propagate in Full Resolution
 
+"""
+read vtk polydata from .vtk file
+"""
 
+def vtk_read_polydata(fn):
+    reader = vtk.vtkPolyDataReader()
+    reader.SetFileName(fn)
+    reader.Update()
+    return reader.GetOutput()
 
--- src/python/run.py
+++ src/python/run.py
@@ -6,7 +6,8 @@ workdir = "/users/jileihao/playground/sandbox"
 outdir = os.path.join(workdir, "out")
 segRefFn = os.path.join(workdir, "seg05_bav07_root_labeled.nii.gz")
 dcmFn = os.path.join(workdir, "bav07.dcm")
-frameNums = [1, 3, 4, 5, 6, 7, 9, 11, 15]
+frameNums = [1, 5, 6, 7]
+
 
 p.propagate( \
     fndcm = dcmFn, \
@@ -16,5 +17,36 @@ p.propagate( \
     framenums = frameNums, \
     fref = 5 \
 )
+
+
+"""
+import vtk_read_polydata as vrp
+meshFn = os.path.join(workdir, "seg05_bav07_root_labeled.vtk")
+reader = vrp.vtk_read_polydata(meshFn)
+poly_data = reader.GetOutput()
+#print(poly_data)
+print(poly_data.GetNumberOfCells())
+"""
+
+"""
+print("POLY DATA --------------------------------------")
+print(poly_data)
+print("STRIPS -------------------------------------")
+strips = poly_data.GetStrips()
+print(strips)
+
+# Restore cell data back to poly data
+poly_data.SetStrips(strips)
+print("RESULT -----------------------------------------")
+print(poly_data)
+
+writer = vtk.vtkPolyDataWriter()
+writer.SetFileName(os.path.join(workdir, "mesh_out.vtk"))
+writer.SetInputData(poly_data)
+writer.Write()
+"""
+
+
+
 
 print("Complete")
